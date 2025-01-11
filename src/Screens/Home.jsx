89db5971c0dbd0d91215c624b22302708028d874
M7MDdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-01-11 14:04:28 +0300

    Add fonts

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-slate-600 mx-auto w-fit">
+      <h1 className="text-5xl text-[#F2F2F2] font-Albert">Mohammed Mahjari</h1>
+      <p className="font-Fira">Software enginner</p>
+      <p className="font-Fira">Obsessed with Web Technologies.</p>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        Albert: ['"Albert_Sans"', "sans-serif"],
+        Fira: ['"Fira_Code"', "sans-serif"],
+      },
+
       backgroundImage: {
         'custom-gradient': 'linear-gradient(180deg, rgba(93, 48, 190, 0.06) 0%, rgba(60, 60, 60, 0.14) 100%), #161616',
       },
