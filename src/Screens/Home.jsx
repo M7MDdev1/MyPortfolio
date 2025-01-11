54af11fab5731cf21458db7d3e533b970a95c047
M7MDdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-01-11 14:20:22 +0300

    Home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-slate-600 mx-auto w-fit">
-      <h1 className="text-5xl text-[#F2F2F2] font-Albert">Mohammed Mahjari</h1>
-      <p className="font-Fira">Software enginner</p>
-      <p className="font-Fira">Obsessed with Web Technologies.</p>
+    <div className="mx-auto w-fit text-[#F2F2F2] flex flex-col justify-center items-start translate-y-[200%]">
+      <h1 className="text-5xl  font-Albert font-bold mb-5">Mohammed Mahjari</h1>
+      <p className="font-Fira font-medium">Software enginner</p>
+      <p className="font-Fira text-[#8F9BA8] font-normal">Obsessed with Web Technologies.</p>
     </div>
   )
 }
