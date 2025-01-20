4fed2cc62799bac86f2929fd65ad9c2f5adb2940
Mohammed27800 <asmte2780@gmail.com>
2025-01-20 08:30:38 +0300

    NavMenu

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -7,33 +7,41 @@ export default function NavMenu({ visibility, setNavMenuVisibility }) {
   };
   return (
     <div
-      className={`flex flex-col items-center justify-center px-12 py-[2.70rem] mx-8 bg-green-700 rounded-[1.25rem] ${
+      className={`flex absolute top-10 flex-col items-center justify-center px-12 py-[2.70rem] mx-8 bg-[#1D192B] rounded-[1.25rem] ${
         !visibility && "hidden"
       }`}
     >
-      <IoIosCloseCircleOutline className="absolute top-0 left-0" color="#AFB0B6" size={40} onClick={()=>handleVisibility}/>
+      <button onClick={handleVisibility}>
+        <IoIosCloseCircleOutline
+          className="absolute top-8 left-8 z-50"
+          color="#AFB0B6"
+          size={40}
+        />
+      </button>
 
-      <img src="/B_Logo.png" alt="Logo" className={"h-16"} />
+      <div className="flex flex-col items-center gap-y-12">
+        <img src="/B_Logo.png" alt="Logo" className={"h-16"} />
 
-      <div className="flex flex-col items-center px-[0.88rem] py-[2.19rem] text-[#AFB0B6] text-[0.875rem] font-Manrope">
-        <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
-          <a href="#home">Home</a>
-        </button>
-        <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
-          <a href="#Portfolio">Portfolio</a>
-        </button>
+        <div className="flex flex-col items-center justify-center gap-y-4 px-[0.88rem] py-[2.19rem] text-[#AFB0B6] text-[0.875rem] font-Manrope">
+          <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
+            <a href="#home">Home</a>
+          </button>
+          <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
+            <a href="#Portfolio">Portfolio</a>
+          </button>
 
-        <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
-          <a href="#aboutme">About me</a>
-        </button>
+          <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
+            <a href="#aboutme">About me</a>
+          </button>
 
-        <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
-          <a href="#contact">Services</a>
-        </button>
-      </div>
+          <button className="py-6 px-[1.88rem]" onClick={handleVisibility}>
+            <a href="#contact">Contact me</a>
+          </button>
+        </div>
 
-      <div className="bg-[#564082] px-[1.25rem] py-[0.88rem] text-white rounded-lg w-56 text-center">
-        <button>Contact Me</button>
+        <div className="bg-[#564082] px-[1.25rem] py-[0.88rem] text-white rounded-lg w-56 text-center">
+          <button>Contact Me</button>
+        </div>
       </div>
     </div>
   );
