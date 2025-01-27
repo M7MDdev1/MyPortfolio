11a4bc1eb0ed0b876ef509cd65e1b86ae8eab85a
Mohammed27800 <asmte2780@gmail.com>
2025-01-27 09:38:06 +0300

    Add skilss

--- src/Pages/Home/Container/Skills.jsx
+++ src/Pages/Home/Container/Skills.jsx
@@ -23,22 +23,23 @@ export default function Skills() {
         Unix
 
         */}
+        <Skill name={"JavaScript"} img={"Skills/js.svg"}/>
+        <Skill name={"TypeScript"} img={"Skills/ts.svg"}/>
+        <Skill name={"React"} img={"Skills/react.svg"}/>
         <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
-        <Skill name={"Figma"} img={"Skills/Figma.png"}/>
+
+        <Skill name={"Firebase"} img={"Skills/Firebase.png"}/>
+        <Skill name={"Git"} img={"Skills/git.svg"}/>
+        <Skill name={"Github"} img={"Skills/Github.png"}/>
+        <Skill name={"Java"} img={"Skills/java.webp"}/>
+        <Skill name={"Laravel"} img={"Skills/laravel.png"}/>
+        <Skill name={"Nest.js"} img={"Skills/nest.svg"}/>
+        <Skill name={"PHP"} img={"Skills/php.png"}/>
+        <Skill name={"PostgreSQL"} img={"Skills/postgresql.svg"}/>
+        <Skill name={"Python"} img={"Skills/Python.png"}/>
+        <Skill name={"React"} img={"Skills/react.svg"}/>
+        <Skill name={"Tailwind"} img={"Skills/Tailwind.svg"}/>
+        <Skill name={"Unix"} img={"Skills/unix.png"}/>
     </div>
   )
 }
