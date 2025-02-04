f7bba67b349f54dfce0b42fb196edb7b97da3fb8
Mohammed27800 <asmte2780@gmail.com>
2025-02-04 14:45:44 +0300

    new NavBar width

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar({ visibility, setVisibility }) {
   return (
     <div
       className={`bg-[linear-gradient(180deg,#000_1.26%,rgba(0,0,0,0.00)100%),radial-gradient(72.83%_40.4%_at_50%_27.17%,#5201E3_0%,rgba(0,0,0,0.50)100%)] rounded-[4.8125rem] border border-[#6D3AFD]
-        flex items-center justify-between h-24 px-8 mb-10
+        flex items-center justify-between h-24 px-8 mb-10 mx-4
         `}
     >
       <img src="W_Logo.png" alt="Logo" className={"h-14"} />
