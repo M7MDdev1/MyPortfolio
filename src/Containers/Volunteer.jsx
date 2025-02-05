65f5340d2f88242a8ea4a8b5f7969a45d833b380
M7MDdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-02-05 16:42:33 +0300

    volanteer margin

--- src/Containers/Volunteer.jsx
+++ src/Containers/Volunteer.jsx
@@ -3,7 +3,7 @@ import VolanteerCard from "../Components/Volanteer-Card";
 
 export default function Volunteer() {
   return (
-    <div className="flex flex-col gap-y-4" id="volunteer">
+    <div className="flex flex-col gap-y-4 mb-14g-" id="volunteer">
       <SubTitle>Experties</SubTitle>
       <VolanteerCard
         img={"Volanteer/Bedar.png"}
