2a293f49516d6456dd44cb18d55e7b80640d5b34
M7mdDev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-06-06 01:57:16 +0300

    improve SEO

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,49 @@ import Skills from "./Containers/Skills";
 import Experties from "./Containers/Experties";
 import Projects from "./Containers/Projects";
 import Volunteer from "./Containers/Volunteer";
+import { Helmet } from "react-helmet";
+
 export default function App() {
   const [navMenuVisibility, setNavMenuVisibility] = useState(false); // Correct state declaration
 
   return (
     <>
+      <Helmet>
+        <title>Mohammed Mahjari – Full-Stack Web Developer</title>
+
+        <meta
+          name="description"
+          content="I’m Mohammed Mahjari, a Saudi Full-Stack Web Developer specializing in React and Laravel. I build fast, modern, and user-friendly web applications."
+        />
+
+        <meta
+          name="keywords"
+          content="محمد مهجري, مبرمج, مطور, React, Laravel, TypeScript, TailwindCSS, سيرة ذاتية, CV, Saudi Arabia, مطور سعودي, مواقع, مشاريع برمجية"
+        />
+
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Mohammed Mahjari" />
+
+        {/* Open Graph - for social media */}
+        <meta property="og:title" content="Mohammed Mahjari – Web Developer" />
+        <meta
+          property="og:description"
+          content="I build scalable, modern web apps using React and Laravel. Contact me through my portfolio or LinkedIn."
+        />
+        <meta property="og:image" content="https://m7md.co/Image/logo.jpg" />
+        <meta property="og:url" content="https://m7md.co" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:locale" content="en_US" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Mohammed Mahjari – Web Developer" />
+        <meta
+          name="twitter:description"
+          content="I build scalable, modern web apps using React and Laravel. Contact me through my portfolio or LinkedIn."
+        />
+      </Helmet>
+
       <div
         dir="rtl"
         className="bg-loading-gradient text-white font-bold min-h-screen justify-center items-center flex-col hidden sm:flex"
